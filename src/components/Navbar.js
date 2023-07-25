713f3bcacf078a05fcc374585a4b36dcd68bd966
YuvalBansal <yuvalbansal200.yb@gmail.com>
2023-07-25 13:29:35 +0530

    Added Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar(props){
+    return(
+        <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <a  href="/" className="navbar-brand">{props.title}</a>
+          <form className="d-flex" role="search">
+            <button className="btn btn-outline-success" type="submit">Logout</button>
+          </form>
+        </div>
+      </nav>
+    )
+}
