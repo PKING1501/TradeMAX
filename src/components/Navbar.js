b4a91f565a6c1cb20190a72ddaca72fa97ac275b
YuvalBansal <yuvalbansal200.yb@gmail.com>
2023-07-25 13:38:50 +0530

    Added Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar(props){
     return(
-        <nav className="navbar bg-body-tertiary">
+        <nav className="navbar bg-primary" data-bs-theme="dark">
         <div className="container-fluid">
           <a  href="/" className="navbar-brand">{props.title}</a>
           <form className="d-flex" role="search">
